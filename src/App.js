8201b8deb7b1336d36c5e6591d25fb1f369aa822
eitan gueron <eitangueron@gmail.com>
2021-02-13 10:36:04 +0200

    updated data structure

--- src/App.js
+++ src/App.js
@@ -7,12 +7,30 @@ import MainBox from './components/main-box';
 
 const App = observer((props) => {
     
-  const [numsArray, setNumsArray] = useState([1,2,3])
+  const [data, setData] = useState({
+    numsArray:[1,2,3],
+    barAmount:20,
+    sortSpeed:1,
+    minBarSize:1,
+    maxBarSize:1000,
+  },)
+
+  // const updateBarAmount = (newAmount) => {
+  //   setData({...data, barAmount:newAmount})
+  // }
+  
+  const setNumsArray = (newArray) => {
+    setData({...data, numsArray: newArray})
+  }
+
+  // const [numsArray, setNumsArray] = useState([1,2,3])
 
   return (
       <div>
         <ControlBar setNumsArray={setNumsArray}/>
-        <MainBox id="main-box" numsArray={numsArray}/>
+        <MainBox id="main-box" numsArray={data.numsArray}/>
+        {/* <ControlBar setNumsArray={setNumsArray}/>
+        <MainBox id="main-box" numsArray={numsArray}/> */}
       </div>
   )
 
--- src/components/main-box.jsx
+++ src/components/main-box.jsx
@@ -5,13 +5,15 @@ const MainBox = (props) => {
 
     return (
        <div style={{border:'solid 1px black', width:'90vw', height:'90vh', marginLeft: "5vw", 
-       display: "grid", alignItems: "center", gridTemplateColumns: "repeat(20, 1fr)"}}>
+       display: "grid", alignItems: "end", gridTemplateColumns: "repeat(20, 1fr)"}}>
             {props.numsArray.map( (n,i) => 
                 <div key={`bar ${n} / ${i}`} 
                 style={{backgroundColor:'#3498db', border:'1px solid black',
-            margin:'10px', display:'inline-block', width:'10px', height:`${n*0.09}vh`,
+            margin:'0 10px', display:'inline-block', width:'10px', height:`${n*0.09}vh`,
             // position: 'relative',
-             top:`calc(88vh - ${n*0.09}vh)` }}> {n} </div>)
+             top:`calc(88vh - ${n*0.09}vh)` ,
+             justifySelf: "center"
+            }}> {n} </div>)
             }
        </div>
     )
