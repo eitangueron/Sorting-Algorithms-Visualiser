efb987e6f9905f00b683a85b0a138eb510d37612
eitan gueron <eitangueron@gmail.com>
2021-02-19 18:15:02 +0200

    bubble sort - func

--- src/algo-helpers/bubbleSort.js
+++ src/algo-helpers/bubbleSort.js
@@ -1,18 +1,22 @@
-// export default 
+export default bubbleSort
 const bubbleSort = (unSortedArr) => {
     const motions = []
-    // {bar1:"",bar2:"",swap:true}
+    // {bar1:"",bar2:"",swapped:true}
     const arr = [...unSortedArr]
     for(let i=0; i<arr.length; i++){
         for(let j=i+1; j<arr.length; j++){
+            let swapped=false
             if(arr[i] > arr[j]){
+                swapped = true
                 let temp = arr[i]
                 arr[i]=arr[j]
                 arr[j]=temp
             }
+            motions.push({bar1:arr[i], bar2:arr[j],swap:swapped})
         }
     }
-    return [arr,motions]
+    return {arr:arr,motions:motions}
 }
+
 
 // console.log( bubbleSort([2,5,4,3,4,55,66,111,3332,4,3]) )
